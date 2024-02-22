74fa9599d79d144bf11573ab0345874252286444
Roopansh Sethi <38037190+roopansh123@users.noreply.github.com>
2024-02-23 00:30:57 +0530

    practice update

--- Backtracking.java
+++ Backtracking.java
@@ -19,10 +19,46 @@ public class Backtracking {
     }
     
     
+    public static void printsubstr(String str, String ans, int i){
+        if(i==str.length()){
+            System.out.println(ans);
+            return;
+        }
+
+        printsubstr(str,ans+str.charAt(i),i+1);
+
+        printsubstr(str, ans, i+1);
+
+    }
+    public static void swap(char ar[],int i, int fi){
+        char temp =ar[i];
+        ar[i]= ar[fi];
+        ar[fi]=temp;
+    }
+
+    public static void permutation(char[]ar, int fi){
+        if(fi == ar.length-1){
+            System.out.println(ar);
+            return;
+        }
+        
+        for(int i=fi;i<ar.length;i++){
+            swap(ar, i, fi);
+            permutation(ar, fi+1);
+            swap(ar, i, fi);
+        }
+
+
+
+    }
     public static void main(String []args){
-        int arr[]=new int[5];
-        changearr(arr, 0, 1);
-        printarr(arr);
+        // int arr[]=new int[5];
+        // changearr(arr, 0, 1);
+        // printarr(arr);
+
+        String str="abc";
+        permutation(str.toCharArray(), 0);
+        // printsubstr(str, "", 0);
 
     }
     
