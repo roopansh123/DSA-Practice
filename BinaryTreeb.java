e9a103fbfd542b5eac73c5bf53228137f3242a11
Roopansh Sethi <38037190+roopansh123@users.noreply.github.com>
2024-04-15 23:45:19 +0530

    update

--- BinaryTreeb.java
+++ BinaryTreeb.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 public class BinaryTreeb {
 
     static class Node{
@@ -59,11 +62,146 @@ public class BinaryTreeb {
         
     }
 
+    public static void levelOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Node currNode =q.remove();
+            if (currNode == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                System.out.print(currNode.data);
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right!=null){
+                    q.add(currNode.right);
+                }
+
+            }
+        }
+
+    }
+
+    public static int height(Node root){
+        if(root == null){
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return Math.max(lh,rh)+1;
+    }
+
+    public static int count(Node root){
+        if(root == null){
+            return 0;
+        }
+        int lc = count(root.left);
+        int rc = count(root.right);
+
+        return lc+rc+1;
+    }
+
+
+    public static int sum(Node root){
+        if(root == null){
+            return 0;
+        }
+        int ls = sum(root.left);
+        int rs = sum(root.right);
+
+        return ls+rs+root.data;
+    }
+
+    public static int Diameter(Node root){
+        if(root == null){
+            return 0;
+        }
+        int ldiam= Diameter(root.left);
+        int lheight = height(root.left);
+        int rdiam = Diameter(root.right);
+        int rheight = height(root.right);
+
+        int currdiam = lheight + rheight+1;
+
+        return Math.max(currdiam, Math.max(rdiam, ldiam));
+
+
+    }
+    public static boolean  isIdentical(Node root, Node subRoot){
+        
+        if (root == null && subRoot == null) {
+            return true ;
+        }
+        else if ( root == null || subRoot  == null || root.data != subRoot.data){
+            return false;
+        }
+        
+        if (!isIdentical(root.left, subRoot.left)) {
+            return false;
+        }
+
+        if (!isIdentical(root.right, subRoot.right)) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    public static boolean isSubtree(Node root , Node subroot){
+
+        if (root == null) {
+            return false;
+        }
+
+        if (subroot.data == root.data) {
+            if (isIdentical(root, subroot)) {
+                return true;
+            }
+        }
+        return isSubtree(root.left, subroot) || isSubtree(root.right, subroot);
+    }
     public static void main(String[] args) {
-        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        BinaryTree t = new BinaryTree();
-        Node root = t.BuildTree(nodes);
-        preorder(root);
+        // int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        // BinaryTree t = new BinaryTree();
+        // Node root = t.BuildTree(nodes);
+        // // levelOrder(root);
+        // // System.out.println(sum(root));
+
+        // System.out.println(Diameter(root));
+
+
+        Node root  = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+
+        Node subRoot = new Node(2);
+        subRoot.left = new Node(5);
+        subRoot.right = new Node(5);
+
+
+        System.out.println(isSubtree(root, subRoot));
+
+        
     }
 
 }
