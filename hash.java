34696e4bf0951971a75692ad48175ade79a7a1e8
Roopansh Sethi <38037190+roopansh123@users.noreply.github.com>
2024-06-23 02:26:32 +0530

    update

--- hash.java
+++ hash.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class hash {
     public static void main(String[] args) {
@@ -7,19 +7,26 @@ public class hash {
         hm.put("China", 190);
         hm.put("Aus", 19);
 
-        System.out.println(hm);
-        // Size
-        System.out.println(hm.size());
+        // System.out.println(hm);
+        // // Size
+        // System.out.println(hm.size());
 
-        // Get --- O(1)
-        System.out.println(hm.get("India"));
+        // // Get --- O(1)
+        // System.out.println(hm.get("India"));
 
-        // Contain Key --- O(1)
-        System.out.println(hm.containsKey("Aus"));
+        // // Contain Key --- O(1)
+        // System.out.println(hm.containsKey("Aus"));
 
-        // Remove -- O(1)
-        hm.remove("India");
-        System.out.println(hm);
+        // // Remove -- O(1)
+        // hm.remove("India");
+        // System.out.println(hm);
+        Set<String> Keys=hm.keySet();
+        
+
+        for (String k: Keys) {
+            System.out.println("key = "+ k +" & " +"Value ="+ hm.get(k));
+            
+        }
 
     }
 }
